844b6b4a936fd7571541dd9e00afafee1a2ab6d9
Echarger1 <echarger1@gmail.com>
2016-06-29 17:23:33 -0400

    Added RegEx

--- Library/Book.cs
+++ Library/Book.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    class Book
+    class Book : Media
     {
     }
 }
--- Library/DVD.cs
+++ Library/DVD.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    class DVD
+    class DVD : Media
     {
+
     }
 }
