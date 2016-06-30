edccf6c356d4cc03dae1f2c5126fbccde1bfd6ea
Echarger1 <echarger1@gmail.com>
2016-06-30 10:46:08 -0400

    Added DVD class

--- Library/DVD.cs
+++ Library/DVD.cs
@@ -8,6 +8,30 @@ namespace Library
 {
     class DVD : Media
     {
+        public override int RentalLength
+        {
+            get
+            {
+                return 7;
+            }
 
+            // Doesn't do anything
+            set
+            {
+            }
+        }
+
+        public override void PrintMediaDetails()
+        {
+            Console.WriteLine("DVD {0}", Title);
+            Console.WriteLine("DVD {0}", Length);
+            Console.WriteLine("DVD {0}", RentendDate);
+            Console.WriteLine("DVD due on: {0}", ReturnDueDate());
+        }
+
+        public override DateTime ReturnDueDate()
+        {
+            return RentendDate.AddDays(RentalLength);
+        }
     }
 }
