3e0f61b5237a45e63faa327419bb7bbfd173e19c
Echarger1 <echarger1@gmail.com>
2016-06-30 13:44:24 -0400

    New Branch

--- Library/Book.cs
+++ Library/Book.cs
@@ -8,5 +8,30 @@ namespace Library
 {
     class Book : Media
     {
+        public override int RentalLength
+        {
+            get
+            {
+                return 21;
+            }
+
+            //Doesn't do anything
+            set
+            {
+            }
+        }
+
+        public override void PrintMediaDetails()
+        {
+            Console.WriteLine("Book {0}", Title);
+            Console.WriteLine("Book {0}", Length);
+            Console.WriteLine("Book {0}", RentendDate);
+            Console.WriteLine("Book due on: {0}", ReturnDueDate()); ;
+        }
+
+        public override DateTime ReturnDueDate()
+        {
+            return RentendDate.AddDays(RentalLength);
+        }
     }
 }
--- Library/Magazine.cs
+++ Library/Magazine.cs
@@ -8,5 +8,30 @@ namespace Library
 {
     class Magazine : Media
     {
+        public override int RentalLength
+        {
+            get
+            {
+                return 12;
+            }
+
+            // Doesn't do anything
+            set
+            {
+            }
+        }
+
+        public override void PrintMediaDetails()
+        {
+            Console.WriteLine("Magazine {0}", Title);
+            Console.WriteLine("Magazine {0}", Length);
+            Console.WriteLine("Magazine {0}", RentendDate);
+            Console.WriteLine("Magazine due on: {0}", ReturnDueDate());
+        }
+
+        public override DateTime ReturnDueDate()
+        {
+            return RentendDate.AddDays(RentalLength);
+        }
     }
 }
